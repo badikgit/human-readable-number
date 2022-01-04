4951bec2ce480914dd8f12832c682938321b5247
badikgit <stas_yar@tut.by>
2022-01-04 22:26:38 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ module.exports = function toReadable(number) {
     let tenName;
     let oneName;
 
-    arrTensNames = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    arr10_19Names = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    arrOnesNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let arrTensNames = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let arr10_19Names = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arrOnesNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
     let numStr = String(number);
     let countDigits = numStr.length;
 
     hundredName = countDigits == 3 ? arrOnesNames[Number(numStr[countDigits-3])] + ' hundred' : '';
-    hundredName += hundredName !== '' && numStr[countDigits-2] !== '0' && numStr[countDigits-1] !== '0' ? ' ' : '';
+    hundredName += hundredName !== '' && (numStr[countDigits-2] !== '0' || numStr[countDigits-1] !== '0') ? ' ' : '';
 
     tenName = countDigits > 1 && numStr[countDigits-2] !== '0' && numStr[countDigits-2] !== '1' ? arrTensNames[Number(numStr[countDigits-2])] : '';
     tenName += tenName !== '' && numStr[countDigits-1] !== '0' ? ' ' : '';
 
     oneName = (tenName !== '' && numStr[countDigits-1] !== '0') ? arrOnesNames[Number(numStr[countDigits-1])] : '';
-    oneName += (countDigits == 1) ? arrOnesNames[Number(numStr[countDigits-1])] : '';
-    oneName += (countDigits == 2 && number > 9 && number < 20) ? arr10_19Names[Number(numStr[countDigits-1])] : '';
+    oneName += (countDigits == 1) || (countDigits == 3 && numStr[countDigits-2] === '0' && numStr[countDigits-1] !== '0') ? arrOnesNames[Number(numStr[countDigits-1])] : '';
+    oneName += (countDigits == 2 && number > 9 && number < 20) || (countDigits > 2 && numStr[countDigits-2] === '1') ? arr10_19Names[Number(numStr[countDigits-1])] : '';
 
 
     return hundredName + tenName + oneName;
